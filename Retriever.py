107192186840f1a90a021461abc1980d42a0a339
yunu-lee <yunu.lee@gmail.com>
2025-03-06 14:59:07 +0900

    add songpa lib retriever

---
+++ Retriever.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+
+class Retriever(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def retrieve(self) -> pd.DataFrame():
+        pass
+
+    @abstractmethod
+    def export(self):
+        # export to artifacts // image, document and etc
+        pass
+
+    # def get_apt_sale_data(self): # 가변형 인자
+    #     pass
+    #
+    # def get_apt_for_sale_data(self):
+    #     pass
---
+++ SongpaLibRetriever.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+from Retriever import Retriever
+
+
+class SongpaLibRetriever(Retriever):
+    def retrieve(self) -> pd.DataFrame():
+
+        cookies = {
+            '_fwb': '89WcExhieiQWyIvv9c8cQi.1740013933943',
+            'JSESSIONID': 'aaahmYqJjrx5m6DR7rNuzt0ZmMnn8QQbqQMABHMaTsdARIUu7uJRQdz00X2F',
+            'wcs_bt': '3a0f999342f0c4:1740244178',
+        }
+
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Language': 'ko,en;q=0.9,en-US;q=0.8',
+            'Connection': 'keep-alive',
+            'Referer': 'https://www.splib.or.kr/spwlib/menu/10409/contents/40184/contents.do',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0',
+            'sec-ch-ua': '"Not(A:Brand";v="99", "Microsoft Edge";v="133", "Chromium";v="133"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            # 'Cookie': '_fwb=89WcExhieiQWyIvv9c8cQi.1740013933943; JSESSIONID=aaahmYqJjrx5m6DR7rNuzt0ZmMnn8QQbqQMABHMaTsdARIUu7uJRQdz00X2F; wcs_bt=3a0f999342f0c4:1740244178',
+        }
+
+        response = requests.get(
+            'https://www.splib.or.kr/spwlib/menu/10406/program/30014/eventList.do',
+            cookies=cookies,
+            headers=headers,
+        )
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        all_programs = []
+
+        for i in range(1, 10):
+            try:
+                article = soup.select_one(
+                    f'#contents > div.articleWrap > div:nth-child({i}) > div.infoBox').text.splitlines()
+                article = [x.strip() for x in article if x]
+                program = {
+                    'host': '송파위례도서관',
+                    'title': article[1],
+                    'audience': article[5],
+                    'price': article[8],
+                    'class_start': article[10],
+                    'class_end': article[12],
+                    'class_day_time': article[13],
+                    'place': article[14],
+                    'register_start': article[16],
+                    'register_end': article[18],
+                }
+                all_programs.append(program)
+            except Exception as e:
+                print(e)
+
+        df = pd.DataFrame(all_programs)
+        df['audience'] = df['audience'].apply(lambda x: x.replace('(', '').replace(')', ''))
+        df['price'] = df['price'].apply(lambda x: x.replace('수강료 :', ''))
+
+        return df
+
+    def export(self):
+        pass
+
+
+if __name__ == '__main__':
+    retriever = SongpaLibRetriever()
+    print(retriever.retrieve())
