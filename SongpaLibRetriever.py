3c2cf74f7643a3f65ce01f07f62606de5d3a293c
yunu-lee <yunu.lee@gmail.com>
2025-03-08 15:36:25 +0900

    add filter

--- SongpaLibRetriever.py
+++ SongpaLibRetriever.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -65,16 +67,25 @@ class SongpaLibRetriever(Retriever):
         df = pd.DataFrame(all_programs)
         df['audience'] = df['audience'].apply(lambda x: x.replace('(', '').replace(')', ''))
         df['price'] = df['price'].apply(lambda x: x.replace('수강료 :', ''))
-
+        df['class_start'] = pd.to_datetime(df['class_start'])
+        df['class_end'] = pd.to_datetime(df['class_end'])
+        df['register_start'] = pd.to_datetime(df['register_start'])
+        df['register_end'] = pd.to_datetime(df['register_end'])
         self.retrieved_data = df
         return df
 
     def export(self):
-        msg = '송파위례도서관 프로그램 상위 2개\n\n'
+        msg = '송파위례도서관 프로그램\n\n'
 
-        for idx, row in self.retrieved_data[0:min(2, len(self.retrieved_data))].iterrows():
-            msg += f"{idx + 1}) {row.get('title')}, {row.get('audience')}, {row.get('price')}, 일정({row.get('class_start')} ~ {row.get('class_end')}, {row.get('class_day_time')}), 등록({row.get('register_start')} ~ {row.get('register_end')})"
-            msg += '\n\n'
+        df = self.retrieved_data[self.retrieved_data['register_end'] > (datetime.now() - timedelta(days=1))]
+
+        if df.empty:
+            msg += '모집중인 프로그램이 없습니다.\n\n'
+        else:
+            for idx, row in df.iterrows():
+                msg += f"{idx + 1}) {row.get('title')}, {row.get('audience')}, {row.get('price')}, 일정({row.get('class_start')} ~ {row.get('class_end')}, {row.get('class_day_time')}), 등록({row.get('register_start')} ~ {row.get('register_end')})"
+                msg += '\n\n'
+
         msg += 'https://www.splib.or.kr/spwlib/menu/10406/program/30014/eventList.do'
 
         data_to_notify = [{
