67c534b2329bf4f3e26e6a39fccadd1f6a021eef
yunu-lee <127168722+yunu-lee@users.noreply.github.com>
2025-04-18 13:11:08 +0900

    Create db_test.py

---
+++ db_test.py
@@ -0,0 +1,9 @@
+import mymongo
+
+ATLAS_URI = os.getenv('ATLAS_URI')
+
+client = pymongo.MongoClient(ATLAS_URI)
+collection = client['real_estate']['apt']
+df = pd.DataFrame(list(collection.find()))
+
+print(df)
